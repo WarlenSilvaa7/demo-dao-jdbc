c56b9e037a81dc6d2ff1bc6d9464f9a7db808a57
Warlen <warlensa007@gmail.com>
2025-05-01 13:54:06 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,9 +31,15 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 3: seller insert =====");
+        System.out.println("\n=== TEST 4: seller insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com",new Date(),4000.0,department);
         selleDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 4: seller insert =====");
+        seller = selleDao.findById(1);
+        seller.setName("Martha Waine");
+        selleDao.update(seller);
+        System.out.println("Update completed");
     }
 }
